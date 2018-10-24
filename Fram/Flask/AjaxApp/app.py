651e7eb5ffb8a6425739db7e74b6056001e619ef
huangshaoqi <290678391@qq.com>
2018-10-24 20:20:33 +0800

    Ajax练习

--- Fram/Flask/AjaxApp/app.py
+++ Fram/Flask/AjaxApp/app.py
@@ -1,13 +1,16 @@
 from flask import Flask,render_template
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-bootstrap = Bootstrap()
-bootstrap.init_app(app)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('1.html')
+
+
+@app.route('/test')
+def test():
+    return render_template('2.html')
 
 
 if __name__ == '__main__':
