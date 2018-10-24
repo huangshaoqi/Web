787bedfed89659dfdb5ab77554bd55884de9ded0
huangshaoqi <290678391@qq.com>
2018-10-24 18:36:09 +0800

    bootstrap练习

--- Fram/Flask/AjaxApp/app.py
+++ Fram/Flask/AjaxApp/app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap()
+bootstrap.init_app(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
