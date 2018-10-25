fa835f840252a7be17e5b376ffb58e0815df8119
huangshaoqi <290678391@qq.com>
2018-10-26 00:54:40 +0800

    小米2

--- Fram/Flask/MiApp/static/js/my.js
+++ Fram/Flask/MiApp/static/js/my.js
@@ -1,20 +1,20 @@
 //导航栏里的下载app效果,二维码显示
-$('.download').mouseover(function () {
-    // console.log("aa");
+$('.download').mouseover(function() {
+    // console.log("a");
     //鼠标移进.download标签时显示
     $('.download .box').css("display", "block");
 });
-$('.download').mouseout(function () {
+$('.download').mouseout(function() {
     //鼠标移出.download标签时显示
     $('.download .box').css("display", "none");
     //鼠标移进.box时显示
-    $('.download .box').mouseover(function () {
-        console.log("bb");
+    $('.download .box').mouseover(function() {
+        // console.log("b");
         $('.download .box').css("display", "block");
     });
     //鼠标移出.box时显示
-    $('.download .box').mouseout(function () {
-        console.log("bb");
+    $('.download .box').mouseout(function() {
+        // console.log("c");
         $('.download .box').css("display", "none");
     });
 });
