3403fea95cebbee371d3702b749832c3de875af0
huangshaoqi <290678391@qq.com>
2018-10-29 00:02:58 +0800

    小米

--- Fram/Flask/MiApp/app.py
+++ Fram/Flask/MiApp/app.py
@@ -6,5 +6,24 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/check',methods=['GET'])
+def check():
+    # 数据库里的账户信息
+    account_info = {'username':'admin','password':'123456'}
+    # 获取用户输入的数值
+    p_username = request.values['username']
+    p_password = request.values['password']
+    print(p_username, p_password)
+    if p_username == account_info['username'] and p_password == account_info['password']:
+        print(1)
+        return "1" #检验通过
+    else:
+        print(0)
+        return "0" #检验不通过
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
